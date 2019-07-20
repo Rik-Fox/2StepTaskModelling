bce8754aa9f34550f4dfbcc611f66795a13e3725
RFox <Rik-Fox>
2019-07-20 15:14:42 +0100

    data import script

--- ModelFitting.jl
+++ ModelFitting.jl
@@ -1,15 +1,35 @@
 push!(LOAD_PATH, pwd())
-using Random, Plots, StepTaskModelling
+using Random, Plots, StepTaskModelling, CSV, DataFrames, Pkg
 pyplot()
 
-habitSimResults = plotSim(runHabit,N=5000,α=0.01)
-MFSimResults = plotSim(runMF,N=5000,α=0.01)
-MBSimResults = plotSim(runMB,N=5000,α=0.01)
-GDSimResults = plotSim(runGD,N=5000,α=0.01)
-HWVSimResults = plotSim(runHWV,N=5000,α=0.05)
+# DataFrame(skipmissing(CSV.File("Events2.csv",delim=','; missingstrings=["", "NA"])))
+rawData = DataFrame(CSV.File("data/Subj3.csv",delim=','))
 
-plot(habitSimResults[6],legend=false)
-plot(MFSimResults[2],legend=false)
-plot(MBSimResults[2],MBSimResults[4],legend=false)
-plot(GDSimResults[2],GDSimResults[4],legend=false)
-plot(HWVSimResults[2],HWVSimResults[4],HWVSimResults[6],HWVSimResults[8],legend=false)
+cleanData = groupby(rawData,:Flex0_or_Spec1)[1]
+names(cleanData)
+
+switch = [t==0.3 for t in cleanData.Transition_Prob]
+
+exActn = Array{Bool,2}([switch cleanData.First_Choice cleanData.Second_Choice])
+
+exRwd = Array{Bool,1}(cleanData.Reward)
+
+exactEx = buildStepTask(2,TM=true)
+
+exActn[4,1]
+
+for i=1:length(switch)
+
+    if (exActn[i,2] == true && exActn[i,1] == false) || (exActn[i,2] == false && exActn[i,1] == true)
+
+
+
+
+experimtentRwdProb = [cleanData.p1 cleanData.p2 cleanData.p3 cleanData.p4]
+
+experimentalActn[34,:]
+
+
+HWV = plotSim(runHWV,N=10000,α=0.01)
+
+HWV[6]
